4ffcfca63026777084df076318201101f45d801e
idwou <iwasiu460@gmail.com>
2022-03-15 09:46:43 +0100

    added the attributes for coordinates of the vertices as well as the sides of the triangle

--- src/com/teeta90array/AreaOfTriangle/AreaOfTriangleApp.java
+++ src/com/teeta90array/AreaOfTriangle/AreaOfTriangleApp.java
@@ -1,13 +1,24 @@
 package com.teeta90array.AreaOfTriangle;
+
+import java.util.Scanner;
+
 /*Write a program that prompts the user to enter three points, (x1, y1) , (x2, y2) , 
   and (x3, y3) , of a triangle then displays its area.
 */
 
 public class AreaOfTriangleApp {
-
+	//	attributes for co-ordinates of the vertices
+	private static double x1,x2,x3,y1,y2,y3;
+	
+	//	sides of the triangle
+	private static double side1,side2,side3;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// create a scanner object
 
+		
+		
+		
 	}
 
 }
