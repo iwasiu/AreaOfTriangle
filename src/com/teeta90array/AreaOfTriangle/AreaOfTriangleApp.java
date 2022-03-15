12e53b69a96eb68028c53d6503662de293cf5cc4
idwou <iwasiu460@gmail.com>
2022-03-15 09:43:17 +0100

    First commit

---
+++ src/com/teeta90array/AreaOfTriangle/AreaOfTriangleApp.java
@@ -0,0 +1,13 @@
+package com.teeta90array.AreaOfTriangle;
+/*Write a program that prompts the user to enter three points, (x1, y1) , (x2, y2) , 
+  and (x3, y3) , of a triangle then displays its area.
+*/
+
+public class AreaOfTriangleApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
